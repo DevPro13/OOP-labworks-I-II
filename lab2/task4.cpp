ccf355e436ccaed507e5165149b3e7e28cce445a
DevPro13 <devrajparajuli38@gmail.com>
2020-10-09 10:27:48 +0545

    outpus of lab1 added

--- lab2/task4.cpp
+++ lab2/task4.cpp
@@ -4,7 +4,28 @@ constructor) and destructor taking class complex as an example.
 */
 #include<iostream>
 using namespace std;
+class Complex
+{
+    int real,imag;
+public:
+    //default constructure
+    Complex():real(0),imag(0){
+        cout<<"Ok. I am a default constructor"<<endl<<"real="
+            <<real<<" ,imag="<<imag<<endl;
+    }
+    //parameterized constructor
+    Complex(int r,int i):real(r),imag(i){
+        cout<<"I am goddamn parameterized constructor"<<endl<<"real="
+            <<real<<" ,imag="<<imag<<endl;;        
+    }
+    copy Complex(){
+         cout<<"Ok. I am a default constructor"<<endl<<"real="
+            <<real<<" ,imag="<<imag<<endl;
+    }
+};
 int main()
 {
+    Complex c;
+    Complex c1(5,7);
     return 0;
 }
